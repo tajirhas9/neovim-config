01f389063d59083b4c9692a2d5c67efd93b0fc37
Syed Tajir Hasnain <tajircuet@gmail.com>
2025-11-24 14:23:17 +0600

    feat(fzf): replaced telescope with fzf

--- lua/tajirhas9/file-finder.lua
+++ lua/tajirhas9/file-finder.lua
@@ -1,25 +1,32 @@
-require'telescope'.setup{
-  defaults = {
-      file_ignore_patterns = {
-          "node_modules",
-          "%.git/",
-          "%.lock",
-          "%.umd.js%"
-      }
-  },
-  pickers = {
-    find_files = {
-      find_command = { "fd", "--type", "f", "--color", "never", "--no-require-git" }
-    }
-  }
-  -- ...
-}
--- search files, even hidden ones
-vim.keymap.set('n', '<leader>ff', ':lua require"telescope.builtin".find_files()<CR>', {})
--- ripgrep files, respects gitignore
-vim.keymap.set('n', '<leader>fg', ':lua require"telescope.builtin".live_grep()<CR>', {})
-vim.keymap.set('n', '<leader>;', ':lua require"telescope.builtin".buffers()<CR>', {})
-vim.keymap.set('n', '<leader>fo', ':lua require"telescope.builtin".oldfiles()<CR>', {})
--- find in current buffers
-vim.keymap.set('n', '<leader>fb',
-    ':lua require"telescope.builtin".find_files({ cwd = require"telescope.utils".buffer_dir() })<CR>', {})
+-- require'telescope'.setup{
+--   defaults = {
+--       file_ignore_patterns = {
+--           "node_modules",
+--           "%.git/",
+--           "%.lock",
+--           "%.umd.js%"
+--       }
+--   },
+--   pickers = {
+--     find_files = {
+--       find_command = { "fd", "--type", "f", "--color", "never", "--no-require-git" }
+--     }
+--   }
+--   -- ...
+-- }
+-- -- search files, even hidden ones
+-- vim.keymap.set('n', '<leader>ff', ':lua require"telescope.builtin".find_files()<CR>', {})
+-- -- ripgrep files, respects gitignore
+-- vim.keymap.set('n', '<leader>fg', ':lua require"telescope.builtin".live_grep()<CR>', {})
+-- vim.keymap.set('n', '<leader>;', ':lua require"telescope.builtin".buffers()<CR>', {})
+-- vim.keymap.set('n', '<leader>fo', ':lua require"telescope.builtin".oldfiles()<CR>', {})
+-- -- find in current buffers
+-- vim.keymap.set('n', '<leader>fb',
+--     ':lua require"telescope.builtin".find_files({ cwd = require"telescope.utils".buffer_dir() })<CR>', {})
+
+
+vim.keymap.set('n', '<leader>ff', ':lua require"fzf-lua".files()<CR>', {})
+vim.keymap.set('n', '<leader>fg', ':lua require"fzf-lua".live_grep()<CR>', {})
+vim.keymap.set('n', '<leader>fm', ':lua require"fzf-lua".marks()<CR>', {})
+vim.keymap.set('n', '<leader>;', ':lua require"fzf-lua".buffers()<CR>', {})
+vim.keymap.set('n', '<leader>fo', ':lua require"fzf-lua".oldfiles()<CR>', {})
--- lua/tajirhas9/lazy.lua
+++ lua/tajirhas9/lazy.lua
@@ -36,11 +36,15 @@ require("lazy").setup({
     "nvim-lualine/lualine.nvim",
     { "nvim-treesitter/nvim-treesitter" --, build = ":TSUpdate"
     },
+    -- {
+    --     "nvim-telescope/telescope.nvim",
+    --     tag = "0.1.6",
+    --     dependencies = { "nvim-lua/plenary.nvim" },
+    --     event = "VeryLazy",
+    -- },
     {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.6",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        event = "VeryLazy",
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
     },
     {
         'windwp/nvim-autopairs',
